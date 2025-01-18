f230a457742761b3e7a964eab1989ea7486af81b
Alid-Tech-Digital-Works <alidsmnp@gmail.com>
2025-01-19 06:53:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("startButton").addEventListener("click", function(event) {
+    // Menampilkan pesan ketika tombol diklik
+    alert("Selamat belajar! Siapkan dirimu untuk langkah pertama.");
+});
